43233801214500f0749e633cc9eca13f2af13b3a
mryunt02 <mrthresh58@gmail.com>
2024-06-19 20:14:42 +0300

    compose explanation

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ const {
 const function1 = (string) => string.toUpperCase();
 const function2 = (string) => string.repeat(3);
 const function3 = (string) => string.bold();
-composeFunction = compose(function1, function2, function3);
+composeFunction = compose(function1, function2, function3); // compose function helps to combine multiple functions into a single function
 
 console.log(composeFunction("Hello"));
