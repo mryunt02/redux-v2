97b8d9c6c1b6a5133fdd9a9f29adbae9fcf40a96
mryunt02 <mrthresh58@gmail.com>
2024-06-19 20:32:09 +0300

    createStore reducer

--- index.js
+++ index.js
@@ -9,5 +9,18 @@ const function1 = (string) => string.toUpperCase();
 const function2 = (string) => string.repeat(3);
 const function3 = (string) => string.bold();
 composeFunction = compose(function1, function2, function3); // compose function helps to combine multiple functions into a single function
+// console.log(composeFunction("Hello")); // HELLOHELLOHELLO
 
-console.log(composeFunction("Hello"));
+// Redux createStore
+const initialState = { value: 0 };
+const increment = { type: "INCREMENT", payload: 3 };
+const reducer = (state, action) => {
+  if (action.type === "INCREMENT") {
+    return { value: state.value + action.payload };
+  }
+  return state;
+}; // //Reducers are functions that take the current state and an action as arguments, and return a new state result. In other words, (state, action) => newState
+const store = createStore(reducer, initialState); // createStore function is used to create a store expect a reducer as the first argument and an initial state as the second argument
+console.log(store.getState()); // { value: 0 }
+store.dispatch(increment);
+console.log(store.getState()); // { value: 3 }
