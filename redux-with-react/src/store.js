c9a4403753552b72ef8414b7b7fb3b2c6ed15581
mryunt02 <mrthresh58@gmail.com>
2024-06-26 22:00:22 +0300

    redux dev tools added as enhancer

--- redux-with-react/src/store.js
+++ redux-with-react/src/store.js
@@ -1,6 +1,8 @@
 import { initialState, reducer } from "./reducer";
-import { createStore } from "redux"; 
+import { createStore } from "redux";
 
-const store = createStore(reducer, initialState);
+const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(reducer, initialState, enhancer);
 
 export { store };
